46231d810437f8eb33b7eee2c1e5d4bccd9f86b6
Chandrakanth0698 <chandrakanth0698@gmail.com>
2023-11-04 15:34:30 -0400

    fix bugs

--- conatct.py
+++ conatct.py
@@ -3,7 +3,7 @@ from send_email import send_email
 import pandas as pd
 
 st.header("Contact Us")
-df = pd.read_csv(r"D:\pythonProject\Codingexercise\topics.csv")
+df = pd.read_csv(r"/topics.csv")
 with st.form(key="contact_form"):
     user_email = st.text_input("Enter Email address")
     topic_selected = st.selectbox(label="Select a topic", options=df['topic'])

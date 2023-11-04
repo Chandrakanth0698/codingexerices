9e821b66acb197d75bf21ede2bf6efe05be7ddeb
Chandrakanth0698 <111915753+Chandrakanth0698@users.noreply.github.com>
2023-11-04 15:26:38 -0400

    Update conatct.py

--- pages/conatct.py
+++ pages/conatct.py
@@ -7,7 +7,7 @@ df = pd.read_csv(r"D:\pythonProject\Codingexercise\topics.csv")
 with st.form(key="contact_form"):
     user_email = st.text_input("Enter Email address")
     topic_selected = st.selectbox(label="Select a topic", options=df['topic'])
-    raw_message = st.text_area("Enter mesaage")
+    raw_message = st.text_area("Enter message")
     message = f"""\
 Subject: Email from company website
 
@@ -17,6 +17,6 @@ message:{raw_message}
     """
     button = st.form_submit_button()
     if button:
-        send_email(message)
+        send_email(message,password=st.secrets['YOUR_SMTP_GMAIL_PASSWORD'])
         st.info("email sent successfully")
 
